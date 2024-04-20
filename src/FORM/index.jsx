2fe1ae909d06d9b63302e379e9c8a17f68de0837
tanluc1403 <tanluc1403@gmail.com>
2024-04-20 23:02:48 +0700

    Done

--- src/FORM/index.jsx
+++ src/FORM/index.jsx
@@ -124,11 +124,11 @@ class Form extends Component {
         // ngăn load lại web khi click vào nút submit
         event.preventDefault();
 
-        const { listUser,editUser } = this.props;
+        const { listUser, editUser } = this.props;
         let newErrors = { ...this.state.errors };
         let isAdd = true;
         const listUserUpdate = listUser.filter(item => item != editUser);
- 
+
         // check id trung lap
         const phoneFind = listUserUpdate.find((user) => {
             return user.phone === this.state.phone;
@@ -136,7 +136,7 @@ class Form extends Component {
         const emailFind = listUserUpdate.find((user) => {
             return user.email === this.state.email;
         });
-        
+
         if (phoneFind) {
             newErrors.phone = "(*) SỐ ĐIỆN THOẠI đã tồn tại";
             isAdd = false;
@@ -147,8 +147,8 @@ class Form extends Component {
         };
 
         this.setState({
-                errors : newErrors
-            })
+            errors: newErrors
+        })
 
         let isValid = true;
         // có thể sử dụng vòng lặp for in
@@ -160,166 +160,199 @@ class Form extends Component {
         if (isValid && isAdd) {
             // cho phép gửi dữ liệu đi
             this.props.updateUser(this.state);
-            this.props.resetUserEdit();
+            // this.props.resetUserEdit();
             this.props.hiddenUpdate();
+            this.setState({
+                id:  "" ,
+                name:  "" ,
+                phone:  "",
+                email:  "",
+                errors: {
+                    id: "",
+                    name: "",
+                    phone: "",
+                    email: "",
+                }
+            })
         } else {
             // alert("Vui lòng kiểm tra lại thông tin");
-            
+
         }
 
     }
+    componentDidUpdate(prevProps) {
 
-    //edit user
-    UNSAFE_componentWillReceiveProps(nextProps) {
+        const { editUser } = prevProps; //nextProps: hiện tất cả các props được truyền qua, nên cần chọn cái nào thì lấy cái đó ra ngoài, (tham số mặc định)
 
-        const { editUser } = nextProps; //nextProps: hiện tất cả các props được truyền qua, nên cần chọn cái nào thì lấy cái đó ra ngoài, (tham số mặc định)
 
-        if (editUser) {
+        if (editUser !== this.props.editUser) {
             this.setState({
-                id: editUser.id,
-                name: editUser.name,
-                phone: editUser.phone,
-                email: editUser.email,
-            })
-        } else {
-            this.setState({
-                id: "",
-                name: "",
-                phone: "",
-                email: "",
+                id:  this.props.editUser?.id ,
+                name:  this.props.editUser?.name ,
+                phone:  this.props.editUser?.phone,
+                email:  this.props.editUser?.email,
+                errors: {
+                    id: "",
+                    name: "",
+                    phone: "",
+                    email: "",
+                }
             })
         }
     }
 
-    handleReset = (event) => {
-        event.preventDefault();
-        this.setState({
-            id: "",
-            name: "",
-            phone: "",
-            email: "",
-            errors: "",
-        })
-    };
-    
-    render() {
 
-        return (
-            <div className='container-fluid '>
-                <div className="row">
-                    <div className="col-6 mx-auto">
-                        <h3 className="display-4 text-center mb-5">Thông Tin Sinh Viên </h3>
+//edit user
+// UNSAFE_componentWillReceiveProps(nextProps) {
 
-                        <form id="formQLSV">
-                            <div className="row">
-                                <div className="col-6 form-group">
-                                    <label htmlFor>Mã Số : </label>
-                                    <input
-                                        type="number"
-                                        className="form-control"
-                                        id="txtMaSV"
-                                        disabled={this.props.hidenButon}
-                                        name="id"
-                                        onChange={this.handleOnchange}
-                                        value={this.state.id}
-                                        onBlur={this.handleOnchange}
-                                        pattern='^\d+$'
-                                    />
-                                    {this.state.errors.id && (
-                                        <span className="text text-danger">{this.state.errors.id}</span>
-                                    )}
-                                </div>
-                                <div className="col-6 form-group">
-                                    <label htmlFor>Họ tên : </label>
-                                    <input
-                                        // type="text"
-                                        className="form-control"
-                                        id="txtTenSV"
-                                        name="name"
-                                        onChange={this.handleOnchange}
-                                        value={this.state.name}
-                                        onBlur={this.handleOnchange}
-                                        pattern="^[a-zA-ZÀ-ỹ\s]+$"
-                                    />
-                                    {this.state.errors.name && (
-                                        <span className="text text-danger">{this.state.errors.name}</span>
-                                    )}
-                                </div>
-                                <div className="col-6 form-group">
-                                    <label htmlFor>Số điện thoại: </label>
-                                    <input
-                                        type="number"
-                                        className="form-control"
-                                        id="txtPass"
-                                        name="phone"
-                                        onChange={this.handleOnchange}
-                                        value={this.state.phone}
-                                        onBlur={this.handleOnchange}
-                                        pattern="^(03|05|07|08|09)\d{8}$"
+//     const { editUser } = nextProps; //nextProps: hiện tất cả các props được truyền qua, nên cần chọn cái nào thì lấy cái đó ra ngoài, (tham số mặc định)
 
-                                    />
-                                    {this.state.errors.phone && (
-                                        <span className="text text-danger">{this.state.errors.phone}</span>
-                                    )}
-                                </div>
-                                <div className="col-6 form-group">
-                                    <label htmlFor>Email : </label>
-                                    <input
-                                        type="email"
-                                        className="form-control"
-                                        id="txtEmail"
-                                        name="email"
-                                        onChange={this.handleOnchange}
-                                        value={this.state.email}
-                                        onBlur={this.handleOnchange}
-                                        pattern="\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
-                                    />
-                                    {this.state.errors.email && (
-                                        <span className="text text-danger">{this.state.errors.email}</span>
-                                    )}
-                                </div>
+//     if (editUser) {
+//         this.setState({
+//             id: editUser.id,
+//             name: editUser.name,
+//             phone: editUser.phone,
+//             email: editUser.email,
+//         })
+//     } else {
+//         this.setState({
+//             id: "",
+//             name: "",
+//             phone: "",
+//             email: "",
+//         })
+//     }
+// }
+
+handleReset = (event) => {
+    event.preventDefault();
+    this.setState({
+        id: "",
+        name: "",
+        phone: "",
+        email: "",
+        errors: "",
+    })
+};
+
+render() {
+
+    return (
+        <div className='container-fluid '>
+            <div className="row">
+                <div className="col-6 mx-auto">
+                    <h3 className="display-4 text-center mb-5">Thông Tin Sinh Viên </h3>
+
+                    <form id="formQLSV">
+                        <div className="row">
+                            <div className="col-6 form-group">
+                                <label htmlFor>Mã Số : </label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    id="txtMaSV"
+                                    disabled={this.props.hidenButon}
+                                    name="id"
+                                    onChange={this.handleOnchange}
+                                    value={this.state.id}
+                                    onBlur={this.handleOnchange}
+                                    pattern='^\d+$'
+                                />
+                                {this.state.errors.id && (
+                                    <span className="text text-danger">{this.state.errors.id}</span>
+                                )}
                             </div>
-                            <div className="form-group text-center">
-                                {this.props.hidenButon === true ?
-                                    <button
-                                        type="button"
-                                        className="btn btn-info mr-3"
-                                        onClick={this.handleSubmitUpdate}
-                                    >
-                                        UP DATE
-                                    </button>
-                                    :
-                                    <button
-                                        type="button"
-                                        className="btn btn-success mr-3"
-                                        onClick={this.handleSubmitAdd}
-
-                                    >
-                                        ADD USER
-                                    </button>
-                                }
-                                {this.props.hidenButon === true ?
-                                     ""
-                                    :
-                                    <button
-                                        type="button"
-                                        className="btn btn-dark mr-3"
-                                        onClick={this.handleReset}
-                                    >
-                                        Reset Form
-                                    </button>
-                                }
+                            <div className="col-6 form-group">
+                                <label htmlFor>Họ tên : </label>
+                                <input
+                                    // type="text"
+                                    className="form-control"
+                                    id="txtTenSV"
+                                    name="name"
+                                    onChange={this.handleOnchange}
+                                    value={this.state.name}
+                                    onBlur={this.handleOnchange}
+                                    pattern="^[a-zA-ZÀ-ỹ\s]+$"
+                                />
+                                {this.state.errors.name && (
+                                    <span className="text text-danger">{this.state.errors.name}</span>
+                                )}
                             </div>
-                        </form>
-                        <Search />
-                    </div>
+                            <div className="col-6 form-group">
+                                <label htmlFor>Số điện thoại: </label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    id="txtPass"
+                                    name="phone"
+                                    onChange={this.handleOnchange}
+                                    value={this.state.phone}
+                                    onBlur={this.handleOnchange}
+                                    pattern="^(03|05|07|08|09)\d{8}$"
 
+                                />
+                                {this.state.errors.phone && (
+                                    <span className="text text-danger">{this.state.errors.phone}</span>
+                                )}
+                            </div>
+                            <div className="col-6 form-group">
+                                <label htmlFor>Email : </label>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="txtEmail"
+                                    name="email"
+                                    onChange={this.handleOnchange}
+                                    value={this.state.email}
+                                    onBlur={this.handleOnchange}
+                                    pattern="\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
+                                />
+                                {this.state.errors.email && (
+                                    <span className="text text-danger">{this.state.errors.email}</span>
+                                )}
+                            </div>
+                        </div>
+                        <div className="form-group text-center">
+                            {this.props.hidenButon === true ?
+                                <button
+                                    type="button"
+                                    className="btn btn-info mr-3"
+                                    onClick={this.handleSubmitUpdate}
+                                >
+                                    UP DATE
+                                </button>
+                                :
+                                <button
+                                    type="button"
+                                    className="btn btn-success mr-3"
+                                    onClick={this.handleSubmitAdd}
+
+                                >
+                                    ADD USER
+                                </button>
+                            }
+                            {this.props.hidenButon === true ?
+                                ""
+                                :
+                                <button
+                                    type="button"
+                                    className="btn btn-dark mr-3"
+                                    onClick={this.handleReset}
+                                >
+                                    Reset Form
+                                </button>
+                            }
+                        </div>
+                    </form>
+                    <Search />
                 </div>
 
-                <Studentmanager />
             </div>
-        )
-    }
+
+            <Studentmanager />
+        </div>
+    )
+}
 }
 
 const mapDispatchToProps = (dispatch) => {
